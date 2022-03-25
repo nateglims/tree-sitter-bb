71036cb8ff48a0cfe25fe131d821e73446ff6c44
Minijackson <minijackson@riseup.net>
2022-03-25 13:25:47 +0100

    add language injection

---
+++ queries/injections.scm
@@ -0,0 +1,9 @@
+(shell_task_declaration (block (code_body) @injection.content (#set! injection.language "bash")))
+(python_task_declaration (block (code_body) @injection.content (#set! injection.language "python")))
+
+(shell_task_declaration (block (code_body) @injection.content (#set! injection.language "bash")))
+(python_task_declaration (block (code_body) @injection.content (#set! injection.language "python")))
+(python_expansion (inline_code) @injection.content (#set! injection.language "python"))
+(python_function) @injection.content (#set! injection.language "python")
+
+(comment) @injection.content (#set! injection.language "comment")
